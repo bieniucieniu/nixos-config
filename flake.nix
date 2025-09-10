3d5da955490b3f53eeba01d5adbfa6f45980c519
bieniucieniu <mikibien@outlook.com>
2025-09-10 18:03:59 +0200

    feat: multi system config

--- flake.nix
+++ flake.nix
@@ -13,13 +13,10 @@
       nixpkgs,
     }:
     let
-      configuration =
+      sharedConfiguration =
         { pkgs, ... }:
         {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
           environment.systemPackages = [
-            pkgs.cocoapods
             pkgs.bottom
             pkgs.vim
             pkgs.bash
@@ -27,7 +24,6 @@
             pkgs.fzf
             pkgs.fnm
             pkgs.gh
-            pkgs.go
             pkgs.lazygit
             pkgs.neovim
             pkgs.ripgrep
@@ -37,28 +33,129 @@
             pkgs.nixfmt-rfc-style
           ];
 
-          # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
-
-          # Enable alternative shell support in nix-darwin.
           programs.fish.enable = true;
           users.users.mikolajbien.shell = pkgs.fish;
-          # Set Git commit hash for darwin-version.
+        };
+      macConfiguration =
+        { pkgs, ... }:
+        {
+          environment.systemPackages = [
+            pkgs.cocoapods
+            pkgs.go
+          ];
           system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
           system.stateVersion = 6;
-
-          # The platform the configuration will be used on.
           nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+      nixosConfiguration =
+        { config, pkgs, ... }:
+        {
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+          boot.kernelPackages = pkgs.linuxPackages_latest;
+
+          networking.hostName = "nixos";
+          networking.networkmanager.enable = true;
+
+          time.timeZone = "Europe/Warsaw";
+
+          i18n.defaultLocale = "en_US.UTF-8";
+          i18n.extraLocaleSettings = {
+            LC_ADDRESS = "pl_PL.UTF-8";
+            LC_IDENTIFICATION = "pl_PL.UTF-8";
+            LC_MEASUREMENT = "pl_PL.UTF-8";
+            LC_MONETARY = "pl_PL.UTF-8";
+            LC_NAME = "pl_PL.UTF-8";
+            LC_NUMERIC = "pl_PL.UTF-8";
+            LC_PAPER = "pl_PL.UTF-8";
+            LC_TELEPHONE = "pl_PL.UTF-8";
+            LC_TIME = "pl_PL.UTF-8";
+          };
+
+          hardware.bluetooth = {
+            enable = true;
+            powerOnBoot = true;
+            settings = {
+              General = {
+                Experimental = true;
+              };
+            };
+          };
+
+          services.xserver.enable = true;
+          services.displayManager.sddm.enable = true;
+          services.desktopManager.plasma6.enable = true;
+
+          services.xserver.xkb = {
+            layout = "us";
+            variant = "";
+          };
+
+          services.printing.enable = true;
+          services.openssh.enable = true;
+          services.avahi.enable = true;
+
+          services.pulseaudio.enable = false;
+          security.rtkit.enable = true;
+          services.pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+          };
+          programs.fish.enable = true;
+          users.users.mikolajbien = {
+            isNormalUser = true;
+            shell = pkgs.fish;
+            description = "mikolajbien";
+            extraGroups = [
+              "networkmanager"
+              "wheel"
+            ];
+            packages = with pkgs; [
+              kdePackages.kate
+            ];
+          };
+
+          environment.systemPackages = with pkgs; [
+            neovim
+            git
+            curl
+            qbittorrent
+            mpv
+            google-chrome
+            ghostty
+          ];
+
+          system.stateVersion = "23.11"; # Or update to "25.05" if you truly mean that channel
         };
     in
     {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#Mikolajs-Mac-mini
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          sharedConfiguration
+          nixosConfiguration
+        ];
+      };
+
       darwinConfigurations."Mikolajs-Mac-mini" = nix-darwin.lib.darwinSystem {
-        modules = [ configuration ];
+        system = "aarch64-darwin"; # As specified in your mac config
+        modules = [
+          sharedConfiguration
+          macConfiguration
+        ];
+      };
+
+      # Global Nix configuration settings, apply to both systems
+      nixConfig = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        allow-unfree = true;
       };
     };
 }
