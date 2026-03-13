b28cab15f0a8202961ca1ed42d720546794d9996
bieniucieniu <mikibien@outlook.com>
2026-03-13 17:20:42 +0100

    feat: downgrade devenv

--- configurations/dev.nix
+++ configurations/dev.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    pkgs.devenv
+    (pkgs.devenv.overrideAttrs (prev: rec {
+      version = "2.0.3";
+      src = pkgs.fetchFromGitHub {
+        inherit (prev.src) owner repo;
+        tag = "v2.0.3";
+        hash = "sha256-1DpF5F7zgOZ7QrRjz23315pUoF532dHnsU/V4UQithk=";
+      };
+      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+        inherit src;
+        hash = "sha256-gZFRbTDPQNKf2msBv9wOavaH1iB1Tk3shYf0/4TSZBQ=";
+      };
+    }))
+
     pkgs.direnv
     pkgs.fnm
     pkgs.gh
